3f5d091b3d4ed84697f38be72e98baa5ae38f6d6
WASEEM ATTAR <waseem_6957@yahoo.com>
2023-01-27 19:11:57 +0530

    Add files via upload

---
+++ decisionmaking/SwitchCaseDemo.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+class SwitchCaseDemo {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		System.out.println("1. Addition");
+		System.out.println("2. Subtraction");
+		System.out.println("3. Multiplication");
+		System.out.println("4. Division");
+		System.out.println("ENTER YOUR CHOICE - ");
+		int choice = s.nextInt();
+
+		int num1,num2,result;
+		
+
+		switch (choice) {
+			case 1:
+				System.out.print("ENTER FIRST NUMBER - ");
+				num1 = s.nextInt();
+				System.out.print("ENTER SECOND NUMBER - ");
+				num2 = s.nextInt();
+				result = num1 + num2;
+				System.out.println("ADDITION - "+result);
+				break;
+
+			case 2:
+				System.out.print("ENTER FIRST NUMBER - ");
+				num1 = s.nextInt();
+				System.out.print("ENTER SECOND NUMBER - ");
+				num2 = s.nextInt();
+				result = num1 - num2;
+				System.out.println("SUBTRACTION - "+result);
+				break;
+
+			case 3:
+				System.out.print("ENTER FIRST NUMBER - ");
+				num1 = s.nextInt();
+				System.out.print("ENTER SECOND NUMBER - ");
+				num2 = s.nextInt();
+				result = num1 * num2;
+				System.out.println("MULTIPLICATION - "+result);
+				break;
+
+			case 4:
+				System.out.print("ENTER FIRST NUMBER - ");
+				num1 = s.nextInt();
+				System.out.print("ENTER SECOND NUMBER - ");
+				num2 = s.nextInt();
+				result = num1 / num2;
+				System.out.println("DIVISION - "+result);
+				break;
+
+			default:
+				System.out.println("INVALID CHOICE.");
+		}
+	}
+}
