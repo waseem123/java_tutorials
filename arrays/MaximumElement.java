bc57b3b5cb3da1230ebfd83af28a3dc38dd35c38
WASEEM ATTAR <waseem_6957@yahoo.com>
2023-02-09 19:41:59 +0530

    Array Operations

---
+++ arrays/MaximumElement.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class MaximumElement {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("ENTER THE VALUE OF N - ");
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+
+		System.out.println("ENTER THE ARRAY ELEMENTS - ");
+		for (int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		/*Actual Logic to Find Maximum Element*/
+		int x  = arr[0];
+		for (int i=1; i<arr.length; i++) {
+			if(x<arr[i]){
+				x = arr[i];
+			}
+		}
+		
+		System.out.println(x+" IS THE MAXIMIMUM VALUE FROM ARRAY.");
+
+	}
+}
+
+
+
+// -9 -8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8 9
